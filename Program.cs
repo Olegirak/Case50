d39fbf5060e91f5aab61abfc49e55e1ac851e82c
Oleg Iraklionov <irakliov@mail.ru>
2023-01-28 16:47:49 +0300

    Case 50 is ready

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции 
+элемента в двумерном массиве, и возвращает значение этого элемента
+ или же указание, что такого элемента нет.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+17 -> такого числа в массиве нет*/
+int GetDataFromUser(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    System.Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    Console.ResetColor();
+    return result;
+}
+
+int[,] get2DDoubleArray(int colLength,
+                        int rowLength,
+                        int start,
+                        int finish)
+{
+    int[,] array = new int[colLength, rowLength];
+    for (int i = 0; i < colLength; i++)
+    {
+        for (int j = 0; j < rowLength; j++)
+        {
+            array[i, j] = new Random().Next(start, finish + 1);
+        }
+    }
+    return array;
+}
+void printInColor(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.Write(data);
+    Console.ResetColor();
+}
+void print2Darray(int[,] array)
+{
+    Console.Write(" \t");
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        printInColor(j + "\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        printInColor(i + "\t");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+string VyborEl(int [,] mas, int ind1,int ind2)
+{
+    string res = string.Empty;
+    if (ind1<mas.GetLength(0) && ind2<mas.GetLength(1))
+    {
+        res = res + mas[ind1,ind2];
+    }
+    else
+    {
+        res = "Такого элемента нет";
+    }
+    return res;
+}
+
+
+
+int n = GetDataFromUser("Введите количество строк");
+int m = GetDataFromUser("Введите количество столбцов");
+int[,] Array = get2DDoubleArray(n, m, 1, 15);
+print2Darray(Array);
+int i = GetDataFromUser("Введите номер строки");
+int j = GetDataFromUser("Введите номер столбца");
+Console.WriteLine(VyborEl(Array,i,j));
